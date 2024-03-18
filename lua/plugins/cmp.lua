930d4d5183da824325df64a39190ee729ef3b4b6
Lian <19876680915@163.com>
2024-03-19 01:24:39 +0800

    完善了cmp文件存在的一些问题

--- lua/plugins/cmp.lua
+++ lua/plugins/cmp.lua
@@ -1,40 +1,78 @@
+-- 定义图标
+local kinds = {
+    Text = "  ",
+    Method = "  ",
+    Function = "  ",
+    Constructor = "  ",
+    Field = "  ",
+    Variable = "  ",
+    Class = "  ",
+    Interface = "  ",
+    Module = "  ",
+    Property = "  ",
+    Unit = "  ",
+    Value = "  ",
+    Enum = "  ",
+    Keyword = "  ",
+    Snippet = "  ",
+    Color = "  ",
+    File = "  ",
+    Reference = "  ",
+    Folder = "  ",
+    EnumMember = "  ",
+    Constant = "  ",
+    Struct = "  ",
+    Event = "  ",
+    Operator = "  ",
+    TypeParameter = "  ",
+};
+
 return {
+    { "onsails/lspkind-nvim" }, -- 
     { "hrsh7th/cmp-buffer" },   -- 缓冲区源插件
     { "hrsh7th/cmp-path" },     -- 路径源插件
     { "hrsh7th/cmp-cmdline" },  -- 命令行源插件
     { "hrsh7th/nvim-cmp",  config = function()
         local cmp = require("cmp");
+        local lspkind = require("lspkind");
+        lspkind.init({ symbol_map = kinds });
         cmp.setup({
-            snippet = {
-                -- 设置代码片段的扩展函数
-                expand = function(args)
-                    -- 如果您使用vsnip，使用vsnip#anonymous函数来扩展代码片段
-                    -- vim.fn["vsnip#anonymous"](args.body);
-                    -- 如果您使用luasnip，取消注释以下行
-                    -- require('luasnip').lsp_expand(args.body)
-                    -- 如果您使用snippy，取消注释以下行
-                    -- require('snippy').expand_snippet(args.body)
-                    -- 如果您使用ultisnips，取消注释以下行
-                    -- vim.fn["UltiSnips#Anon"](args.body)
-                end,
+            formatting = {
+                format = lspkind.cmp_format({
+                    mode = 'symbol', -- 显示图标和文本
+                    maxwidth = 100, -- 补全菜单的最大宽度
+                    before = function(entry, vim_item)
+                        vim_item.menu = ({
+                            buffer = "[Buffer]",
+                            nvim_lsp = "[LSP]",
+                            path = "[Path]",
+                            cmdline = "[Cmdline]",
+                            -- 其他补全源的菜单标签...
+                        })[entry.source.name] or vim_item.menu
+                        return vim_item
+                    end,
+                })
             },
             window = {
                 -- 设置补全窗口的样式为有边框
                 completion = cmp.config.window.bordered(),
                 -- 设置文档窗口的样式为有边框
-                -- documentation = cmp.config.window.bordered(),
+                documentation = cmp.config.window.bordered(),
             },
             mapping = cmp.mapping.preset.insert({
-                -- 设置向下滚动文档的按键映射
-                ['<C-b>'] = cmp.mapping.scroll_docs(-4),
-                -- 设置向上滚动文档的按键映射
-                ['<C-f>'] = cmp.mapping.scroll_docs(4),
-                -- 设置触发补全的按键映射
-                ['<C-Space>'] = cmp.mapping.complete(),
-                -- 设置取消补全的按键映射
-                ['<C-e>'] = cmp.mapping.abort(),
-                -- 设置确认选择的补全项的按键映射
-                ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                -- 出现补全
+                ["<A-.>"] = cmp.mapping(cmp.mapping.complete(), {"i", "c"}),
+                -- 取消
+                ["<A-,>"] = cmp.mapping({ i = cmp.mapping.abort(), c = cmp.mapping.close()}),
+                -- 上一个
+                ["<C-k>"] = cmp.mapping.select_prev_item(),
+                -- 下一个
+                ["<C-j>"] = cmp.mapping.select_next_item(),
+                -- 确认
+                ['<CR>'] = cmp.mapping.confirm({ behavior = cmp.ConfirmBehavior.None }),
+                -- 如果窗口内容太多，可以滚动
+                ["<C-u>"] = cmp.mapping(cmp.mapping.scroll_docs(-4), {"i", "c"}),
+                ["<C-d>"] = cmp.mapping(cmp.mapping.scroll_docs(4), {"i", "c"}),
             }),
             sources = cmp.config.sources({
                 -- 添加LSP源
