5be57aaff764195364b242b34b1afe6886417261
Lian <19876680915@163.com>
2024-03-21 00:07:28 +0800

    修复了一些错误

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -2,10 +2,27 @@ return {
     {
         "folke/noice.nvim",
         event = "VeryLazy",
-        opts = {},
         dependencies = {
             "MunifTanjim/nui.nvim",
             "rcarriga/nvim-notify",
         },
+        config = function()
+            vim.cmd([[ highlight NotifyBackground guibg=#000000 ctermbg=NONE ]]);
+            require("noice").setup({
+                lsp = {
+                    override = {
+                        ["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+                        ["vim.lsp.util.stylize_markdown"] = true,
+                        ["cmp.entry.get_documentation"] = true,
+                    },
+                },
+                presets = {
+                    bottom_search = true,
+                    long_message_to_split = true,
+                    inc_rename = false,
+                    lsp_doc_border = false,
+                },
+            });
+        end,
     },
 };
