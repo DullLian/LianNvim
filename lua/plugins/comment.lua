a5dfc249f91df211377e58352c92fb1234fa8a13
Lian <19876680915@163.com>
2024-03-29 03:18:44 +0800

    修改了comment的按键映射

--- lua/plugins/comment.lua
+++ lua/plugins/comment.lua
@@ -10,32 +10,25 @@ return {
             -- NORMAL模式下的切换注释的快捷键
             toggler = {
                 -- 行注释的切换快捷键
-                line = "gcc",
+                line = "<leader>hz",
                 -- 块注释的切换快捷键
-                block = "gbc",
-            },
-            -- NORMAL和VISUAL模式下的操作符等待模式下的快捷键
-            opleader = {
-                -- 行注释的快捷键
-                line = "gc",
-                -- 块注释的快捷键
-                block = "gb",
+                block = "<leader>hs",
             },
             -- 额外的快捷键映射
             extra = {
                 -- 在光标上方添加注释
-                above = "gcO",
+                above = "<leader>hk",
                 -- 在光标下方添加注释
-                below = "gco",
+                below = "<leader>hj",
                 -- 在行尾添加注释
-                eol = "gcA",
+                eol = "<leader>he",
             },
             -- 启用快捷键映射
             -- 注意：如果设置为false，插件不会创建任何映射
             mappings = {
-                -- 启用基本操作符等待模式映射，如 'gcc' 'gbc' 'gc[count]{motion}' 'gb[count]{motion}'
+                -- 启用基本操作符等待模式映射
                 basic = true,
-                -- 启用额外映射，如 'gco', 'gcO', 'gcA'
+                -- 启用额外映射
                 extra = true,
             },
             -- 注释前调用的函数
