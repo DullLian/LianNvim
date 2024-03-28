3e6e7f3c29ea35cc668f079119ed6bca0d19a0dd
Lian <19876680915@163.com>
2024-03-29 04:57:07 +0800

    修改windows的按键映射

--- lua/plugins/windows.lua
+++ lua/plugins/windows.lua
@@ -1,10 +1,14 @@
 local function cmd(command)
     return table.concat({ "<Cmd>", command, "<CR>" });
 end
-vim.keymap.set("n", "<C-c>z", cmd "WindowsMaximize");
-vim.keymap.set("n", "<C-c>_", cmd "WindowsMaximizeVertically");
-vim.keymap.set("n", "<C-c>|", cmd "WindowsMaximizeHorizontally");
-vim.keymap.set("n", "<C-c>=", cmd "WindowsEqualize");
+-- 最大化窗口
+vim.keymap.set("n", "<leader>wz", cmd("WindowsMaximize"));
+-- 垂直最大化当前窗口的宽度。
+vim.keymap.set("n", "<leader>w_", cmd("WindowsMaximizeVertically"));
+-- 水平最大化当前窗口的高度。
+vim.keymap.set("n", "<leader>w|", cmd("WindowsMaximizeHorizontally"));
+-- 使所有窗口的高度和宽度相等。
+vim.keymap.set("n", "<leader>w=", cmd("WindowsEqualize"));
 
 return {
     { "anuvyklack/windows.nvim",
